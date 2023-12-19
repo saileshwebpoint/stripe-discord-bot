cd12e33c0734deb8d13961564c9147b52a766147
Sailesh Kasaju <saileshkasaju@Saileshs-MacBook-Pro.local>
2023-12-19 11:35:58 +0545

    fix stripe api setup

--- src/integrations/stripe.ts
+++ src/integrations/stripe.ts
@@ -1,4 +1,4 @@
-import pQueue from 'p-queue';
+import pQueue from "p-queue";
 
 const queue = new pQueue({ concurrency: 1, interval: 1000, intervalCap: 1 });
 
@@ -6,83 +6,139 @@ const queue = new pQueue({ concurrency: 1, interval: 1000, intervalCap: 1 });
  * Gets the Stripe customer ID for a given user email
  */
 export const resolveCustomerIdFromEmail = async (email: string) => {
-    let customerData;
-    
-    if (email.includes('+')) {
-        const endPart = email.split('+')[1];
-        const customers = await queue.add(async () => await (await fetch(`https://api.stripe.com/v1/customers/search?query=email~'${endPart}'`, {
-            headers: {
-                Authorization: `Bearer ${process.env.STRIPE_API_KEY}`
-            }
-        })).json());
-        const matchingCustomers = customers.data.filter((c: any) => c.email === email);
-        customerData = matchingCustomers[0];
-    } else {
-        const customers = await queue.add(async () => await (await fetch(`https://api.stripe.com/v1/customers/search?query=email:'${email}'`, {
-            headers: {
-                Authorization: `Bearer ${process.env.STRIPE_API_KEY}`
-            }
-        })).json());
-        customerData = customers.data[0];
-    }
+  let customerData;
 
-    return customerData?.id;
-}
+  if (email.includes("+")) {
+    const endPart = email.split("+")[1];
+    const customers = await queue.add(
+      async () =>
+        await (
+          await fetch(
+            `https://api.stripe.com/v1/customers/search?query=email~'${endPart}'`,
+            {
+              headers: {
+                Authorization: `Bearer ${process.env.STRIPE_API_KEY}`,
+                "Stripe-Version": "2020-08-27",
+              },
+            }
+          )
+        ).json()
+    );
+    const matchingCustomers = customers.data.filter(
+      (c: any) => c.email === email
+    );
+    customerData = matchingCustomers[0];
+  } else {
+    const customers = await queue.add(
+      async () =>
+        await (
+          await fetch(
+            `https://api.stripe.com/v1/customers/search?query=email:'${email}'`,
+            {
+              headers: {
+                Authorization: `Bearer ${process.env.STRIPE_API_KEY}`,
+                "Stripe-Version": "2020-08-27",
+              },
+            }
+          )
+        ).json()
+    );
+    customerData = customers.data[0];
+  }
+
+  return customerData?.id;
+};
 
 /**
  * Gets all the Stripe subscriptions from a given customer ID
  */
-export const findSubscriptionsFromCustomerId = async (oldCustomerId: string) => {
-    const subscriptions = await queue.add(async () => await (await fetch(`https://api.stripe.com/v1/subscriptions?customer=${oldCustomerId}`, {
-        headers: {
-            Authorization: `Bearer ${process.env.STRIPE_API_KEY}`
-        }
-    })).json());
-    return subscriptions.data || [];
-}
+export const findSubscriptionsFromCustomerId = async (
+  oldCustomerId: string
+) => {
+  const subscriptions = await queue.add(
+    async () =>
+      await (
+        await fetch(
+          `https://api.stripe.com/v1/subscriptions?customer=${oldCustomerId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.STRIPE_API_KEY}`,
+              "Stripe-Version": "2020-08-27",
+            },
+          }
+        )
+      ).json()
+  );
+  return subscriptions.data || [];
+};
 
 /**
  * Filter the active subscriptions from a list of subscriptions
  */
 export const findActiveSubscriptions = (subscriptions: any[]) => {
-    return subscriptions.filter(sub => sub.status === 'active' || sub.status === 'trialing' || (sub.cancel_at && sub.current_period_end > Date.now() / 1000));
-}
+  return subscriptions.filter(
+    (sub) =>
+      sub.status === "active" ||
+      sub.status === "trialing" ||
+      (sub.cancel_at && sub.current_period_end > Date.now() / 1000)
+  );
+};
 
 /**
  * Cancels a subscription
  */
 export const cancelSubscription = async (subscriptionId: string) => {
-    await queue.add(async () => await (await fetch(`https://api.stripe.com/v1/subscriptions/${subscriptionId}`, {
-        method: 'DELETE',
-        headers: {
-            Authorization: `Bearer ${process.env.STRIPE_API_KEY}`
-        }
-    })).json());
-}
+  await queue.add(
+    async () =>
+      await (
+        await fetch(
+          `https://api.stripe.com/v1/subscriptions/${subscriptionId}`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${process.env.STRIPE_API_KEY}`,
+              "Stripe-Version": "2020-08-27",
+            },
+          }
+        )
+      ).json()
+  );
+};
 
 /**
  * Gets all the Stripe payments from a given customer ID
  */
 export const getCustomerPayments = async (customerId: string) => {
-    const invoices = await queue.add(async () => await (await fetch(`https://api.stripe.com/v1/payment_intents?customer=${customerId}`, {
-        headers: {
-            Authorization: `Bearer ${process.env.STRIPE_API_KEY}`
-        }
-    })).json());
-    return invoices?.data || [];
-}
+  const invoices = await queue.add(
+    async () =>
+      await (
+        await fetch(
+          `https://api.stripe.com/v1/payment_intents?customer=${customerId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.STRIPE_API_KEY}`,
+              "Stripe-Version": "2020-08-27",
+            },
+          }
+        )
+      ).json()
+  );
+  return invoices?.data || [];
+};
 
 /**
  * Gets the lifetime payment date from a list of payments
  */
-export const getLifetimePaymentDate = (payments: any[]): null|number => {
-    let lifetimeStartDate = null;
-    for (const payment of (payments || [])) {
-        for (const charge of (payment.charges?.data || [])) {
-            if (charge.description.includes(process.env.LIFETIME_INVOICE_LABEL_KEYWORD)) {
-                lifetimeStartDate = charge.created * 1000;
-            }
-        }
+export const getLifetimePaymentDate = (payments: any[]): null | number => {
+  let lifetimeStartDate = null;
+  for (const payment of payments || []) {
+    for (const charge of payment.charges?.data || []) {
+      if (
+        charge.description.includes(process.env.LIFETIME_INVOICE_LABEL_KEYWORD)
+      ) {
+        lifetimeStartDate = charge.created * 1000;
+      }
     }
-    return lifetimeStartDate;
-}
+  }
+  return lifetimeStartDate;
+};
