9e5da50afbd7bc7bb8eb40ad415d0e6d24f365dd
Sailesh Kasaju <saileshkasaju@Saileshs-MacBook-Pro.local>
2024-01-13 09:00:11 +0545

    change implementation of cancel command to remove role instead; prettier

--- src/slash-commands/cancel.ts
+++ src/slash-commands/cancel.ts
@@ -1,133 +1,156 @@
 import { SlashCommandRunFunction } from "../handlers/commands";
 import { errorEmbed, successEmbed } from "../util";
 import { Postgres, DiscordCustomer } from "../database";
-import { ActionRowBuilder, ApplicationCommandOptionType, ButtonBuilder, ButtonInteraction, ButtonStyle, EmbedBuilder, PermissionsBitField } from "discord.js";
-import { cancelSubscription, findActiveSubscriptions, findSubscriptionsFromCustomerId, getCustomerPayments, getLifetimePaymentDate, resolveCustomerIdFromEmail } from "../integrations/stripe";
+import {
+  ActionRowBuilder,
+  ApplicationCommandOptionType,
+  ButtonBuilder,
+  ButtonInteraction,
+  ButtonStyle,
+  EmbedBuilder,
+  GuildMember,
+  PermissionsBitField,
+} from "discord.js";
+import {
+  cancelSubscription,
+  findActiveSubscriptions,
+  findSubscriptionsFromCustomerId,
+  getCustomerPayments,
+  getLifetimePaymentDate,
+  resolveCustomerIdFromEmail,
+} from "../integrations/stripe";
 
 export const commands = [
-    {
-        name: "cancel",
-        description: "Cancel your current subscription",
-        options: [
-            {
-                name: "user",
-                description: "The user you want to cancel the subscription for",
-                type: ApplicationCommandOptionType.User,
-                required: false
-            }
-        ]
-    }
+  {
+    name: "cancel",
+    description: "Cancel your current subscription",
+    options: [
+      {
+        name: "user",
+        description: "The user you want to cancel the subscription for",
+        type: ApplicationCommandOptionType.User,
+        required: false,
+      },
+    ],
+  },
 ];
 
 export const run: SlashCommandRunFunction = async (interaction) => {
-
-    if (interaction.channelId !== process.env.CANCEL_CHANNEL_ID) {
-        return void interaction.reply({
-            content: `This command can only be used in <#${process.env.CANCEL_CHANNEL_ID}>. Please go there and try again.`,
-            ephemeral: true
-        });
-    }
-
-    const user = interaction.options.getUser("user") || interaction.user;
-
-    if (interaction.options.getUser("user") && !interaction.memberPermissions!.has(PermissionsBitField.Flags.Administrator)) {
-        return void interaction.reply({
-            content: `You don't have the permission to cancel someone else's subscription.`,
-            ephemeral: true
-        });
-    }
-
-    const discordCustomer = await Postgres.getRepository(DiscordCustomer).findOne({
-        where: {
-            discordUserId: user.id
-        }
+  if (interaction.channelId !== process.env.CANCEL_CHANNEL_ID) {
+    return void interaction.reply({
+      content: `This command can only be used in <#${process.env.CANCEL_CHANNEL_ID}>. Please go there and try again.`,
+      ephemeral: true,
     });
+  }
 
-    if (!discordCustomer) {
-        return void interaction.reply({
-            ephemeral: true,
-            embeds: errorEmbed(`There is no email linked to your account!`).embeds
-        });
+  const user = interaction.options.getUser("user") || interaction.user;
+
+  if (
+    interaction.options.getUser("user") &&
+    !interaction.memberPermissions!.has(PermissionsBitField.Flags.Administrator)
+  ) {
+    return void interaction.reply({
+      content: `You don't have the permission to cancel someone else's subscription.`,
+      ephemeral: true,
+    });
+  }
+
+  const discordCustomer = await Postgres.getRepository(DiscordCustomer).findOne(
+    {
+      where: {
+        discordUserId: user.id,
+      },
     }
+  );
 
-    const customerId = await resolveCustomerIdFromEmail(discordCustomer.email);
+  if (!discordCustomer) {
+    return void interaction.reply({
+      ephemeral: true,
+      embeds: errorEmbed(`There is no email linked to your account!`).embeds,
+    });
+  }
 
-    const subscriptions = await findSubscriptionsFromCustomerId(customerId);
-    const actives = findActiveSubscriptions(subscriptions);
+  const customerId = await resolveCustomerIdFromEmail(discordCustomer.email);
 
-    const cancelled = actives[0];
+  const subscriptions = await findSubscriptionsFromCustomerId(customerId);
+  const actives = findActiveSubscriptions(subscriptions);
 
-    if (!cancelled) {
-        return void interaction.reply({
-            ephemeral: true,
-            embeds: errorEmbed(`You don't have an active subscription!`).embeds
-        });
-    }
+  const cancelled = actives[0];
 
-    const confirmEmbed = new EmbedBuilder()
-        .setAuthor({
+  if (!cancelled) {
+    return void interaction.reply({
+      ephemeral: true,
+      embeds: errorEmbed(`You don't have an active subscription!`).embeds,
+    });
+  }
+
+  const confirmEmbed = new EmbedBuilder()
+    .setAuthor({
+      name: `${user.tag} cancellation`,
+      iconURL: user.displayAvatarURL(),
+    })
+    .setDescription(`Are you sure you want to cancel your subscription?`)
+    .setColor(process.env.EMBED_COLOR);
+
+  const random3DigitsId = Math.floor(Math.random() * 900) + 10;
+
+  const components = [
+    new ActionRowBuilder<ButtonBuilder>().addComponents([
+      new ButtonBuilder()
+        .setCustomId(`cancel-confirm-${user.id}-${random3DigitsId}`)
+        .setLabel("Confirm")
+        .setStyle(ButtonStyle.Danger),
+    ]),
+  ];
+
+  await interaction.reply({
+    ephemeral: true,
+    embeds: [confirmEmbed],
+    components,
+  });
+
+  const collector = interaction.channel!.createMessageComponentCollector({
+    filter: (i) =>
+      (i as ButtonInteraction)?.customId ===
+      `cancel-confirm-${user.id}-${random3DigitsId}`,
+    time: 1000 * 60 * 5,
+  });
+
+  collector.on("collect", async (_i) => {
+    const i = _i as ButtonInteraction;
+
+    if (i.isButton()) {
+      if (i.customId === `cancel-confirm-${user.id}-${random3DigitsId}`) {
+        const embed = new EmbedBuilder()
+          .setAuthor({
             name: `${user.tag} cancellation`,
-            iconURL: user.displayAvatarURL()
-        })
-        .setDescription(`Are you sure you want to cancel your subscription?`)
-        .setColor(process.env.EMBED_COLOR);
+            iconURL: user.displayAvatarURL(),
+          })
+          .setDescription(
+            `We're sorry to see you go! Your subscription has been cancelled.`
+          )
+          .setColor(process.env.EMBED_COLOR);
 
-    const random3DigitsId = Math.floor(Math.random() * 900) + 10;
-
-    const components = [
-        new ActionRowBuilder<ButtonBuilder>()
-            .addComponents([
-                new ButtonBuilder()
-                    .setCustomId(`cancel-confirm-${user.id}-${random3DigitsId}`)
-                    .setLabel("Confirm")
-                    .setStyle(ButtonStyle.Danger)
-            ])
-    ];
-
-    await interaction.reply({
-        ephemeral: true,
-        embeds: [confirmEmbed],
-        components
-    });
-
-    const collector = interaction.channel!.createMessageComponentCollector({
-        filter: (i) => (i as ButtonInteraction)?.customId === `cancel-confirm-${user.id}-${random3DigitsId}`,
-        time: 1000 * 60 * 5
-    });
-
-    collector.on('collect', (_i) => {
-
-        const i = _i as ButtonInteraction;
-
-        if (i.isButton()) {
-
-            if (i.customId === `cancel-confirm-${user.id}-${random3DigitsId}`) {
-                const embed = new EmbedBuilder()
-                .setAuthor({
-                    name: `${user.tag} cancellation`,
-                    iconURL: user.displayAvatarURL()
-                })
-                .setDescription(`We're sorry to see you go! Your subscription has been cancelled.`)
-                .setColor(process.env.EMBED_COLOR);
-
-                i.reply({
-                    ephemeral: true,
-                    embeds: [embed],
-                    components: []
-                });
-
-                cancelSubscription(cancelled.id);
-            }
-
-        }
-
-    });
-
-    collector.on('end', () => {
-        interaction.editReply({
-            embeds: [confirmEmbed],
-            components: []
+        i.reply({
+          ephemeral: true,
+          embeds: [embed],
+          components: [],
         });
-    });
+        const member = await interaction.guild?.members
+          .fetch(user.id)
+          ?.catch(() => {});
+        await (member as GuildMember)?.roles
+          .remove(process.env.PAYING_ROLE_ID)
+          ?.catch(() => {});
+        // cancelSubscription(cancelled.id);
+      }
+    }
+  });
 
-}
+  collector.on("end", () => {
+    interaction.editReply({
+      embeds: [confirmEmbed],
+      components: [],
+    });
+  });
+};
